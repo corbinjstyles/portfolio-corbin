6ace73f94f43def0bf2869677ac7f60bb27ef625
Corbin Styles <corbin.styles1@gmail.com>
2025-01-03 19:40:06 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,29 @@ import Footnotes from "./components/Footnotes";
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden overflow-y-hidden text-netural-400 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+    <div className="overflow-x-hidden overflow-y-hidden text-neutral-900 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen 
+        <div
+          className="absolute top-0 z-[-2] h-screen w-screen 
           bg-[#000000] 
           bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] 
-          bg-[size:20px_20px]"/>
+          bg-[size:20px_20px]"
+        />
       </div>
       <div className="container mx-auto px-8">
         <Navbar />
-        <Hero />
-        <About />
-        <Technologies/>
-        <Experience/>
-        <Projects/>
-        <Contact/>
-        <Footnotes/>
-      </div> 
+        <div className="grid">
+          <div className="grid-cols-4">
+            <Hero />
+            <About />
+          </div>
+        </div>
+        <Technologies />
+        <Experience />
+        <Projects />
+        <Contact />
+        <Footnotes />
+      </div>
     </div>
   );
 };
